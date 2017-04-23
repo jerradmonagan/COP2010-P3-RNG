15beac3b32ada688fd52b734ebca7bc6db6239e5
jerradmonagan <jerradmonagan@gmail.com>
2017-04-23 16:00:49 -0400

    Uploaded for Historical review

---
+++ Project3jmonagan/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Project3jmonagan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLowerBound = new System.Windows.Forms.TextBox();
+            this.txtUpperBound = new System.Windows.Forms.TextBox();
+            this.txtNumberGenerate = new System.Windows.Forms.TextBox();
+            this.btnGenerateNumber = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lstDisplay = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lower Bound";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 42);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Upper Bound";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(61, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(357, 42);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number to Generate";
+            // 
+            // txtLowerBound
+            // 
+            this.txtLowerBound.Location = new System.Drawing.Point(527, 76);
+            this.txtLowerBound.Name = "txtLowerBound";
+            this.txtLowerBound.Size = new System.Drawing.Size(298, 31);
+            this.txtLowerBound.TabIndex = 3;
+            // 
+            // txtUpperBound
+            // 
+            this.txtUpperBound.Location = new System.Drawing.Point(527, 180);
+            this.txtUpperBound.Name = "txtUpperBound";
+            this.txtUpperBound.Size = new System.Drawing.Size(298, 31);
+            this.txtUpperBound.TabIndex = 4;
+            // 
+            // txtNumberGenerate
+            // 
+            this.txtNumberGenerate.Location = new System.Drawing.Point(527, 281);
+            this.txtNumberGenerate.Name = "txtNumberGenerate";
+            this.txtNumberGenerate.Size = new System.Drawing.Size(298, 31);
+            this.txtNumberGenerate.TabIndex = 5;
+            // 
+            // btnGenerateNumber
+            // 
+            this.btnGenerateNumber.Location = new System.Drawing.Point(970, 64);
+            this.btnGenerateNumber.Name = "btnGenerateNumber";
+            this.btnGenerateNumber.Size = new System.Drawing.Size(244, 42);
+            this.btnGenerateNumber.TabIndex = 6;
+            this.btnGenerateNumber.Text = "&Generate Numbers";
+            this.btnGenerateNumber.UseVisualStyleBackColor = true;
+            this.btnGenerateNumber.Click += new System.EventHandler(this.btnGenerateNumber_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(970, 169);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(244, 42);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(970, 269);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(244, 42);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lstDisplay
+            // 
+            this.lstDisplay.FormattingEnabled = true;
+            this.lstDisplay.ItemHeight = 25;
+            this.lstDisplay.Location = new System.Drawing.Point(225, 383);
+            this.lstDisplay.Name = "lstDisplay";
+            this.lstDisplay.Size = new System.Drawing.Size(989, 379);
+            this.lstDisplay.TabIndex = 9;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1319, 40);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generateToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 36);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // generateToolStripMenuItem
+            // 
+            this.generateToolStripMenuItem.Name = "generateToolStripMenuItem";
+            this.generateToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.G)));
+            this.generateToolStripMenuItem.Size = new System.Drawing.Size(288, 38);
+            this.generateToolStripMenuItem.Text = "&Generate";
+            this.generateToolStripMenuItem.Click += new System.EventHandler(this.btnGenerateNumber_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.R)));
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(288, 38);
+            this.resetToolStripMenuItem.Text = "&Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.X)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(288, 38);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 831);
+            this.Controls.Add(this.lstDisplay);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnGenerateNumber);
+            this.Controls.Add(this.txtNumberGenerate);
+            this.Controls.Add(this.txtUpperBound);
+            this.Controls.Add(this.txtLowerBound);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Random Number Generator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLowerBound;
+        private System.Windows.Forms.TextBox txtUpperBound;
+        private System.Windows.Forms.TextBox txtNumberGenerate;
+        private System.Windows.Forms.Button btnGenerateNumber;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lstDisplay;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+    }
+}
+
---
+++ Project3jmonagan/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project3jmonagan
+{
+    //Jerrad Monagan
+    //COP2010
+    //Project3
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int intLowerBound = 0;
+        int intUpperBound = 0;
+        int intMax;
+        
+        private Boolean ValidateNumber()
+        {
+            intLowerBound = int.Parse(txtLowerBound.Text);
+            intUpperBound = int.Parse(txtUpperBound.Text);
+            intMax = int.Parse(txtNumberGenerate.Text);
+            if ((intLowerBound < intUpperBound)&(intMax>=1))
+                return true;
+            else
+                return false;
+        }//end ValidateNumber
+
+        private void SetBounds()
+        {
+            try
+            {
+                intLowerBound = int.Parse(txtLowerBound.Text);
+                intUpperBound = int.Parse(txtUpperBound.Text);
+                intMax = int.Parse(txtNumberGenerate.Text);
+                int[] countArray = new int[(intUpperBound - intLowerBound) + 1];
+                int[] rngArray = new int[intMax];
+                int[] displayArray=new int[(intUpperBound - intLowerBound) + 1];
+                int intSetIndex = intLowerBound;
+
+                Random rng = new Random();
+                if (ValidateNumber())
+                    for (int intI = 0; intI < (intUpperBound - intLowerBound) + 1; intI++)
+                    {
+                        countArray[intI] = intSetIndex;
+                        intSetIndex++;
+                    }//end if
+
+                else
+                    MessageBox.Show("Lower Bounds must be less than or equal to Upper Bounds and numbers to generate must be greater than 1", "Invalid Input",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                for (int intJ = 0; intJ < rngArray.Length; intJ++)
+                {
+                    int intGenNumber = rng.Next(intLowerBound, intUpperBound);
+                    rngArray[intJ] = intGenNumber;
+                }//end for
+                for(int intK=0; intK < countArray.Length-1; intK++)
+                    for (int intL=0; intL< rngArray.Length; intL++)
+                        if ((countArray[intK]).Equals(rngArray[intL]))
+                        {
+                        displayArray[intK] = (displayArray[intK])+1;
+                        }//end if           
+                for (int intK = 0; intK < countArray.Length; intK++)
+                    lstDisplay.Items.Add("The number "+countArray[intK]+" has been selected "+displayArray[intK]);
+                
+            }//end try
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }//end catch
+        }//end setBounds
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }//end btnExit
+
+        private void btnGenerateNumber_Click(object sender, EventArgs e)
+        {
+            SetBounds();
+        }//end btnGenerateNumber
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtNumberGenerate.Clear();
+            lstDisplay.Items.Clear();
+            txtNumberGenerate.Focus();
+        }//end btnReset
+    }
+}
